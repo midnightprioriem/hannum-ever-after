416390fff77f842c493dafc3bee4bd533c94b78c
Zach Hannum <zachhannum@Zachs-MacBook-Pro.local>
2021-03-15 16:55:56 -0400

    Add hero image/text

---
+++ src/Hero.js
@@ -0,0 +1,15 @@
+
+const Hero = () => {
+    const className = 'hero';
+
+    return (
+        <div className={className}>
+            <div className="heroText">
+                <h1 className="heroTitle" >Bella & Zach</h1>
+                <p className="heroSubtitle">NOVEMBER 6<sup>th</sup>, 2021 • WILMINGTON, NC</p>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
