5956d76873de3ff4cbe8cd8716aaf7a5951c7ed6
Zach Hannum <zachhannum@Zachs-MacBook-Pro.local>
2021-03-15 17:30:57 -0400

    Add RSVP button

--- src/Hero.js
+++ src/Hero.js
@@ -7,6 +7,13 @@ const Hero = () => {
             <div className="heroText">
                 <h1 className="heroTitle" >Bella & Zach</h1>
                 <p className="heroSubtitle">NOVEMBER 6<sup>th</sup>, 2021 • WILMINGTON, NC</p>
+                <button class="btn">
+                    <svg width="90px" height="45px" viewBox="0 0 90 45" class="border">
+                        <polyline points="89,1 89,44 1,44 1,1 89,1" class="bg-line" />
+                        <polyline points="89,1 89,44 1,44 1,1 89,1" class="hl-line" />
+                    </svg>
+                    <p>RSVP</p>
+                </button>
             </div>
         </div>
     );
